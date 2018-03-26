42466dc35dee4aab1156a3b27913d14a50bce548
Matheus Mortatti <matheusmortatti@gmail.com>
2018-03-26 19:51:36 -0300

    refactor math functions + specular highlights

--- maths.cpp
+++ maths.cpp
@@ -2,39 +2,29 @@
 
 #pragma omp declare target
 
-float *scale_vec(float *v, float t) {
-  float *res = new float[3];
-
+void scale_vec(float v[], float t, float res[]) {
   res[0] = v[0] * t;
   res[1] = v[1] * t;
   res[2] = v[2] * t;
-
-  return res;
 }
 
-float *add_vec(float *v1, float *v2) {
-  float *res = new float[3];
-
+void add_vec(float v1[], float v2[], float res[]) {
   res[0] = v1[0] + v2[0];
   res[1] = v1[1] + v2[1];
   res[2] = v1[2] + v2[2];
-
-  return res;
 }
 
-float *sub_vec(float *v1, float *v2) {
-  float *res = new float[3];
-
+void sub_vec(float v1[], float v2[], float res[]) {
   res[0] = v1[0] - v2[0];
   res[1] = v1[1] - v2[1];
   res[2] = v1[2] - v2[2];
-
-  return res;
 }
 
-float length(float *v) { return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]); }
+float length(float v[]) {
+  return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+}
 
-void normalize(float *v) {
+void normalize(float v[]) {
   float l = length(v);
 
   if (l == 0.0)
@@ -46,31 +36,22 @@ void normalize(float *v) {
   }
 }
 
-float *normal(float *p1, float *p2, float *p3) {
-  float *a, *b, *c;
+void normal(float p1[], float p2[], float p3[], float res[]) {
+  float a[3], b[3];
 
-  a = sub_vec(p2, p1);
-  b = sub_vec(p3, p1);
-  c = cross_product(a, b);
-  normalize(c);
-
-  delete[] a;
-  delete[] b;
-
-  return c;
+  sub_vec(p2, p1, a);
+  sub_vec(p3, p1, b);
+  cross_product(a, b, res);
+  normalize(res);
 }
 
-float *cross_product(float *v1, float *v2) {
-  float *res = new float[3];
-
+void cross_product(float v1[], float v2[], float res[]) {
   res[0] = v1[1] * v2[2] - v1[2] * v2[1];
   res[1] = v1[2] * v2[0] - v1[0] * v2[2];
   res[2] = v1[0] * v2[1] - v1[1] * v2[0];
-
-  return res;
 }
 
-float dot_product(float *v1, float *v2) {
+float dot_product(float v1[], float v2[]) {
   return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
@@ -80,92 +61,69 @@ void swap(float *a, float *b) {
   *a = temp;
 }
 
-int rayTriangleIntersects(float *orig, float *dir, float *p1, float *p2,
-                          float *p3, float *P) {
+int rayTriangleIntersects(float orig[], float dir[], float p1[], float p2[],
+                          float p3[], float P[]) {
   float kEpsilon = 0.0001;
 
-  float *N = normal(p1, p2, p3);
+  float N[3];
+  normal(p1, p2, p3, N);
   float NdotRayDirection = dot_product(N, dir);
 
   // Se o produto escalar for quase zero, são paralelos
   if (fabs(NdotRayDirection) < kEpsilon) {
-    delete[] N;
     return false;
   }
 
   float d = dot_product(N, p1);
   float t = (dot_product(N, orig) + d) / NdotRayDirection;
   if (t < 0) {
-    delete[] N;
     return false;
   }
 
-  float *scl = scale_vec(dir, t);
-  float *add = add_vec(orig, scl);
+  float scl[3];
+  scale_vec(dir, t, scl);
+  float add[3];
+  add_vec(orig, scl, add);
   copy_array(P, add, 3);
-  delete[] scl;
-  delete[] add;
 
-  float *C;
+  float C[3];
 
-  float *edge0 = sub_vec(p2, p1);
-  float *vp0 = sub_vec(P, p1);
-  C = cross_product(edge0, vp0);
+  float edge0[3];
+  sub_vec(p2, p1, edge0);
+  float vp0[3];
+  sub_vec(P, p1, vp0);
+  cross_product(edge0, vp0, C);
   if (dot_product(N, C) < 0) {
-    delete[] C;
-    delete[] N;
-    delete[] edge0;
-    delete[] vp0;
-    return false;
-  }
-  delete[] C;
-
-  float *edge1 = sub_vec(p3, p2);
-  float *vp1 = sub_vec(P, p2);
-  C = cross_product(edge1, vp1);
-  if (dot_product(N, C) < 0) {
-    delete[] C;
-    delete[] N;
-    delete[] edge0;
-    delete[] vp0;
-    delete[] edge1;
-    delete[] vp1;
-    return false;
-  }
-  delete[] C;
-
-  float *edge2 = sub_vec(p1, p3);
-  float *vp2 = sub_vec(P, p3);
-  C = cross_product(edge2, vp2);
-  if (dot_product(N, C) < 0) {
-    delete[] C;
-    delete[] N;
-    delete[] edge0;
-    delete[] vp0;
-    delete[] edge1;
-    delete[] vp1;
-    delete[] edge2;
-    delete[] vp2;
     return false;
   }
 
-  delete[] C;
-  delete[] N;
-  delete[] edge0;
-  delete[] vp0;
-  delete[] edge1;
-  delete[] vp1;
-  delete[] edge2;
-  delete[] vp2;
+  float edge1[3];
+  sub_vec(p3, p2, edge1);
+  float vp1[3];
+  sub_vec(P, p2, vp1);
+  cross_product(edge1, vp1, C);
+  if (dot_product(N, C) < 0) {
+    return false;
+  }
+
+  float edge2[3];
+  sub_vec(p1, p3, edge2);
+  float vp2[3];
+  sub_vec(P, p3, vp2);
+  cross_product(edge2, vp2, C);
+  if (dot_product(N, C) < 0) {
+    return false;
+  }
 
   return true;
 }
 
-int raySphereIntersects(float *orig, float *dir, float *s_orig, float radius,
-                        float *P) {
+int raySphereIntersects(float orig[], float dir[], float s_orig[], float radius,
+                        float P[]) {
   float t0, t1;
   float kEpsilon = 0.001;
-  float *L = sub_vec(orig, s_orig);
+  float L[3];
+  sub_vec(orig, s_orig, L);
   float a = dot_product(dir, dir);
   float b = 2 * dot_product(dir, L);
   float c = dot_product(L, L) - radius * radius;
@@ -173,7 +131,6 @@ int raySphereIntersects(float *orig, float *dir, float *s_orig, float radius,
   // Solve quadratic equation
   float discr = b * b - 4 * a * c;
   if (discr < 0) {
-    delete[] L;
     return false;
   } else if (discr == 0)
     t0 = t1 = -0.5 * b / a;
@@ -190,24 +147,24 @@ int raySphereIntersects(float *orig, float *dir, float *s_orig, float radius,
   if (t0 <= kEpsilon) {
     t0 = t1;
     if (t0 <= kEpsilon) {
-      delete[] L;
       return false;
     }
   }
 
-  float *scl = scale_vec(dir, t0);
-  float *add = add_vec(orig, scl);
+  float scl[3];
+  scale_vec(dir, t0, scl);
+  float add[3];
+  add_vec(orig, scl, add);
   copy_array(P, add, 3);
 
-  delete[] scl;
-  delete[] add;
-  delete[] L;
   return true;
 }
 
-void copy_array(float *to, float *from, int size) {
-  for (int i = 0; i < size; i++)
-    to[i] = from[i];
+void reflect(float dir[], float normal[], float refl[]) {
+  float dot = dot_product(dir, normal);
+  float m[3];
+  scale_vec(normal, -2 * dot, m);
+  add_vec(m, dir, refl);
 }
 #pragma omp end declare target
 
